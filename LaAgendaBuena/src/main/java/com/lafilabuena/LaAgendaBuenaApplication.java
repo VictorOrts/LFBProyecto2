657ae876554d78d84d9cdfcf011eae35546929b5
josshb <45046956+josshb@users.noreply.github.com>
2018-12-04 17:29:47 +0100

    TO pa ti papi

---
+++ LaAgendaBuena/src/main/java/com/lafilabuena/LaAgendaBuenaApplication.java
@@ -0,0 +1,12 @@
+package com.lafilabuena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaAgendaBuenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaAgendaBuenaApplication.class, args);
+	}
+}
---
+++ LaAgendaBuena/src/test/java/com/lafilabuena/LaAgendaBuenaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lafilabuena;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LaAgendaBuenaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
