16ed2a098c9f943207a5bae358b8ab4801292d50
Victor Orts <vctr1990@gmail.com>
2018-12-05 14:32:58 +0100

    Contacto
    aaa

---
+++ AgendaBuena/src/main/java/com/lafilabuena/model/Contacto.java
@@ -0,0 +1,142 @@
+/**
+* Contacto Class
+* 
+* @author Victor Orts
+* @version 2.0
+*/
+
+package com.lafilabuena.model;
+
+public class Contacto {
+	private int id;
+	private String nombre;
+	private String apellido1;
+	private String apellido2;
+	private String dni;
+	private int fechanacimiento;
+	private Direccion direccion;
+	public Contacto() {
+		
+	}
+	
+	
+	public Contacto(int id, String nombre, String apellido1, String apellido2, String dni, int fechanacimiento,
+			Direccion direccion) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.apellido1 = apellido1;
+		this.apellido2 = apellido2;
+		this.dni = dni;
+		this.fechanacimiento = fechanacimiento;
+		this.direccion = direccion;
+	}
+
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	/**
+	 * @return the apellido1
+	 */
+	public String getApellido1() {
+		return apellido1;
+	}
+	/**
+	 * @param apellido1 the apellido1 to set
+	 */
+	public void setApellido1(String apellido1) {
+		this.apellido1 = apellido1;
+	}
+	/**
+	 * @return the apellido2
+	 */
+	public String getApellido2() {
+		return apellido2;
+	}
+	/**
+	 * @param apellido2 the apellido2 to set
+	 */
+	public void setApellido2(String apellido2) {
+		this.apellido2 = apellido2;
+	}
+	/**
+	 * @return the dni
+	 */
+	public String getDni() {
+		return dni;
+	}
+	/**
+	 * @param dni the dni to set
+	 */
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	/**
+	 * @return the fechanacimiento
+	 */
+	public int getFechanacimiento() {
+		return fechanacimiento;
+	}
+	/**
+	 * @param fechanacimiento the fechanacimiento to set
+	 */
+	public void setFechanacimiento(int fechanacimiento) {
+		this.fechanacimiento = fechanacimiento;
+	}
+	/**
+	 * @return the direccion
+	 */
+	public Direccion getDireccion() {
+		return direccion;
+	}
+	/**
+	 * @param direccion the direccion to set
+	 */
+	public void setDireccion(Direccion direccion) {
+		this.direccion = direccion;
+	}
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Contacto [id=");
+		builder.append(id);
+		builder.append(", nombre=");
+		builder.append(nombre);
+		builder.append(", apellido1=");
+		builder.append(apellido1);
+		builder.append(", apellido2=");
+		builder.append(apellido2);
+		builder.append(", dni=");
+		builder.append(dni);
+		builder.append(", fechanacimiento=");
+		builder.append(fechanacimiento);
+		builder.append(", direccion=");
+		builder.append(direccion);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+	
+}
