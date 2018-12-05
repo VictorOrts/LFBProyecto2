6e1336e43c5d4c6b311d61a5c74fda635f0ed386
Jorge Av <jrg_aam@hotmail.com>
2018-12-05 12:04:50 +0100

    deee

---
+++ AgendaBuena/src/main/java/com/lafilabuena/model/Direccion.java
@@ -0,0 +1,14 @@
+package com.lafilabuena.model;
+/**
+ * 
+ * @author Jorge A Avilés Menéndez
+ *
+ */
+public class Direccion {
+
+	private String domicilio;
+	
+	private int codigoPostal;
+	
+	
+}
