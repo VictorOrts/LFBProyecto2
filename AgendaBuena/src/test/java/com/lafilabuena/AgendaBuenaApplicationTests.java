5b9addd83ae36a2d320d00434b2e6d01af2059c6
josshb <45046956+josshb@users.noreply.github.com>
2018-12-05 11:54:37 +0100

    P.ad

--- AgendaBuena/src/test/java/com/lafilabuena/AgendaBuenaApplicationTests.java
+++ AgendaBuena/src/test/java/com/lafilabuena/AgendaBuenaApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class LaAgendaBuenaApplicationTests {
+public class AgendaBuenaApplicationTests {
 
 	@Test
 	public void contextLoads() {
