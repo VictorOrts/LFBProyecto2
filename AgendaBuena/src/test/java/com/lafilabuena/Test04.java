ac4d99f0ae355cb504feac24246978b5b5d20406
MarioAlcob <marioalcob@gmail.com>
2018-12-11 17:09:39 +0100

    a

---
+++ AgendaBuena/src/test/java/com/lafilabuena/Test04.java
@@ -0,0 +1,33 @@
+/**
+ * Test para encontrar usuarios tras una búsqueda
+ * 
+ * @author Mario Alcañiz
+ * fecha: 11/12/18
+ * version 1.0
+ *
+ */
+
+package com.lafilabuena;
+
+
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import com.lafilabuena.model.Contacto;
+
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Test04 {
+	
+	List<Contacto> Victor;
+	
+	
+	
+
+}
