bec2b74852aaac5c747b7b1bb4090a4c48654e46
Jorge Av <jrg_aam@hotmail.com>
2018-12-11 12:40:44 +0100

    Creacion clase provincia y terminar clase contacto

--- AgendaBuena/src/main/java/com/lafilabuena/model/Contacto.java
+++ AgendaBuena/src/main/java/com/lafilabuena/model/Contacto.java
@@ -50,15 +50,35 @@ public class Contacto {
 	private int telefono4;
 	@Column(name="telefono5")
 	private int telefono5;
-	//@Column(name="direccion")
-	//private Direccion direccion;
+	@Column(name="direccion")
+	private String direccion;
+	@Column(name="provincia")
+	private Provincias provincia;
+	
 	public Contacto() {
 		
 	}
-	
-	
-	public Contacto(int id, String nombre, String apellido1, String apellido2, String dni, Date fechanacimiento
-			/*Direccion direccion*/) {
+
+	/**
+	 * @param id
+	 * @param nombre
+	 * @param apellido1
+	 * @param apellido2
+	 * @param dni
+	 * @param fechanacimiento
+	 * @param telefono1
+	 * @param telefono2
+	 * @param telefono3
+	 * @param telefono4
+	 * @param telefono5
+	 * @param direccion
+	 * @param provincia
+	 */
+	public Contacto(int id,
+			@NotEmpty(message = "Introduce un nombre, por favor") @Length(min = 3, message = "Tu nombre debe tener al menos 3 caracteres") String nombre,
+			String apellido1, String apellido2, String dni, Date fechanacimiento,
+			@NotEmpty(message = "Introduce un teléfono, por favor") @Length(min = 9, message = "Tu teléfono debe tener 9 dígitos") int telefono1,
+			int telefono2, int telefono3, int telefono4, int telefono5, String direccion, Provincias provincia) {
 		super();
 		this.id = id;
 		this.nombre = nombre;
@@ -66,98 +86,202 @@ public class Contacto {
 		this.apellido2 = apellido2;
 		this.dni = dni;
 		this.fechanacimiento = fechanacimiento;
-		//this.direccion = direccion;
+		this.telefono1 = telefono1;
+		this.telefono2 = telefono2;
+		this.telefono3 = telefono3;
+		this.telefono4 = telefono4;
+		this.telefono5 = telefono5;
+		this.direccion = direccion;
+		this.provincia = provincia;
 	}
-
 
 	/**
 	 * @return the id
 	 */
 	@Id
 	@GeneratedValue(strategy= GenerationType.AUTO)
-    @Column(name="idpersona")
 	public int getId() {
 		return id;
 	}
+
 	/**
 	 * @param id the id to set
 	 */
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	/**
 	 * @return the nombre
 	 */
 	public String getNombre() {
 		return nombre;
 	}
+
 	/**
 	 * @param nombre the nombre to set
 	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	/**
 	 * @return the apellido1
 	 */
 	public String getApellido1() {
 		return apellido1;
 	}
+
 	/**
 	 * @param apellido1 the apellido1 to set
 	 */
 	public void setApellido1(String apellido1) {
 		this.apellido1 = apellido1;
 	}
+
 	/**
 	 * @return the apellido2
 	 */
 	public String getApellido2() {
 		return apellido2;
 	}
+
 	/**
 	 * @param apellido2 the apellido2 to set
 	 */
 	public void setApellido2(String apellido2) {
 		this.apellido2 = apellido2;
 	}
+
 	/**
 	 * @return the dni
 	 */
 	public String getDni() {
 		return dni;
 	}
+
 	/**
 	 * @param dni the dni to set
 	 */
 	public void setDni(String dni) {
 		this.dni = dni;
 	}
+
 	/**
 	 * @return the fechanacimiento
 	 */
 	public Date getFechanacimiento() {
 		return fechanacimiento;
 	}
+
 	/**
 	 * @param fechanacimiento the fechanacimiento to set
 	 */
 	public void setFechanacimiento(Date fechanacimiento) {
 		this.fechanacimiento = fechanacimiento;
 	}
+
+	/**
+	 * @return the telefono1
+	 */
+	public int getTelefono1() {
+		return telefono1;
+	}
+
+	/**
+	 * @param telefono1 the telefono1 to set
+	 */
+	public void setTelefono1(int telefono1) {
+		this.telefono1 = telefono1;
+	}
+
+	/**
+	 * @return the telefono2
+	 */
+	public int getTelefono2() {
+		return telefono2;
+	}
+
+	/**
+	 * @param telefono2 the telefono2 to set
+	 */
+	public void setTelefono2(int telefono2) {
+		this.telefono2 = telefono2;
+	}
+
+	/**
+	 * @return the telefono3
+	 */
+	public int getTelefono3() {
+		return telefono3;
+	}
+
+	/**
+	 * @param telefono3 the telefono3 to set
+	 */
+	public void setTelefono3(int telefono3) {
+		this.telefono3 = telefono3;
+	}
+
+	/**
+	 * @return the telefono4
+	 */
+	public int getTelefono4() {
+		return telefono4;
+	}
+
+	/**
+	 * @param telefono4 the telefono4 to set
+	 */
+	public void setTelefono4(int telefono4) {
+		this.telefono4 = telefono4;
+	}
+
+	/**
+	 * @return the telefono5
+	 */
+	public int getTelefono5() {
+		return telefono5;
+	}
+
+	/**
+	 * @param telefono5 the telefono5 to set
+	 */
+	public void setTelefono5(int telefono5) {
+		this.telefono5 = telefono5;
+	}
+
 	/**
 	 * @return the direccion
 	 */
-//	public Direccion getDireccion() {
-//		return direccion;
-//	}
-//	/**
-//	 * @param direccion the direccion to set
-//	 */
-//	
-//	public void setDireccion(Direccion direccion) {
-//		this.direccion = direccion;
-//	}
+	public String getDireccion() {
+		return direccion;
+	}
+
+	/**
+	 * @param direccion the direccion to set
+	 */
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	/**
+	 * @return the provincia
+	 */
+	public Provincias getProvincia() {
+		return provincia;
+	}
+
+	/**
+	 * @param provincia the provincia to set
+	 */
+	public void setProvincia(Provincias provincia) {
+		this.provincia = provincia;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
@@ -173,11 +297,26 @@ public class Contacto {
 		builder.append(dni);
 		builder.append(", fechanacimiento=");
 		builder.append(fechanacimiento);
-		//builder.append(", direccion=");
-		//builder.append(direccion);
+		builder.append(", telefono1=");
+		builder.append(telefono1);
+		builder.append(", telefono2=");
+		builder.append(telefono2);
+		builder.append(", telefono3=");
+		builder.append(telefono3);
+		builder.append(", telefono4=");
+		builder.append(telefono4);
+		builder.append(", telefono5=");
+		builder.append(telefono5);
+		builder.append(", direccion=");
+		builder.append(direccion);
+		builder.append(", provincia=");
+		builder.append(provincia);
 		builder.append("]");
 		return builder.toString();
 	}
+	
+	
+	
 	
 	
 }
--- AgendaBuena/src/main/java/com/lafilabuena/model/Provincias.java
+++ AgendaBuena/src/main/java/com/lafilabuena/model/Provincias.java
@@ -1,42 +1,89 @@
 package com.lafilabuena.model;
-/**
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**Clase Provincias
  * 
- * Provincias enum
- * 
- * @author Mario Alcañiz
- * fecha: 05/12/18
- * version 1.0
+ * @author Jorge A Avilés Menéndez
  *
  */
-public enum Provincias {
 
-			A_Coruña(15), Álava (01), Albacete (02), Alicante (03), Almería(04), Asturias(33), Avila(05),
-            Badajoz(06), Baleares(07), Barcelona(8), Burgos(9),
-            Cáceres(10), Cadiz(11), Cantabria(39), Castellon(12), Ceuta(51), Ciudad_Real(13), Córdoba(14), Cuenca(16),
-            Girona(17), Granada(18), Guadalajara(19), Guipúzcoa(20),
-            Huelva(21), Huesca(22),
-            Jaén(23),
-            La_Rioja(26), Las_Palmas(35), León(24), Lleida(25), Lugo(27),
-            Madrid(28), Málaga(29), Melilla(52), Murcia(30),
-            Navarra(31),
-            Orense(32),
-            Palencia(34), Pontevedra(36),
-            Salamanca(37), Segovia(40), Sevilla(41), Soria(42),
-            Tarragona(43), Tenerife(38), Teruel(44), Toledo(45),
-            Valencia(46), Valladolid(47), Vizcaya(48),
-            Zamora(49), Zaragoza(50);
-            
-           
-            private final int provincia;
-            
-            Provincias (int provincia) {
-                this.provincia = provincia;
-            }
-            
-            public int getLevelCode() {
-                return this.provincia;
-            }
-            
+
+@Entity
+@Table(name="provincias")
+public class Provincias {
+
+	@Id
+	@GeneratedValue(strategy= GenerationType.AUTO)
+	@Column(name="idprovincia")
+	private int id;
+	@Column(name="provincia")
+	private String provincia;
 	
+	public Provincias() {
+		
+	}
 
+	/**
+	 * @param id
+	 * @param provincia
+	 */
+	public Provincias(int id, String provincia) {
+		super();
+		this.id = id;
+		this.provincia = provincia;
+	}
+
+	/**
+	 * @return the id
+	 */
+	@Id
+	@GeneratedValue(strategy= GenerationType.AUTO)
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the provincia
+	 */
+	public String getProvincia() {
+		return provincia;
+	}
+
+	/**
+	 * @param provincia the provincia to set
+	 */
+	public void setProvincia(String provincia) {
+		this.provincia = provincia;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Provincias [id=");
+		builder.append(id);
+		builder.append(", provincia=");
+		builder.append(provincia);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+	
+	
+	
 }
