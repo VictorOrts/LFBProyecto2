c818cc35498349aa4bd4ac98a275055e4b20c900
MarioAlcob <marioalcob@gmail.com>
2018-12-11 16:49:15 +0100

    r

--- AgendaBuena/src/test/java/com/lafilabuena/Test02.java
+++ AgendaBuena/src/test/java/com/lafilabuena/Test02.java
@@ -10,18 +10,11 @@
 package com.lafilabuena;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertThat;
-
-import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import com.lafilabuena.controller.ContactosController;
 import com.lafilabuena.model.Contacto;
 
 
