8ba3749d4abf043eb5b88703c5d5491d343dde1d
Jorge Av <jrg_aam@hotmail.com>
2018-12-12 16:50:00 +0100

    update rest

--- AgendaBuena/src/main/java/com/lafilabuena/controller/ContactController.java
+++ AgendaBuena/src/main/java/com/lafilabuena/controller/ContactController.java
@@ -1,5 +1,7 @@
 package com.lafilabuena.controller;
-
+/**Clase ContactController
+ * @author Jorge A Avilés Menéndez
+ */
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
